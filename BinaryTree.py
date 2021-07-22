42f9d2be49d71b91117367d9b60824bc7dccf578
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2021-07-22 17:24:39 -0400

    size function not finished

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,37 +1,90 @@
 class Node:
-  # here we will add the constructor
-  # and the string method
-  pass
+  # here we will add the constructor 
+  def __init__(self, data): # override method
+    self.data = data
+    self.left = None
+    self.right = None
+  # method overried of the string method for the node 
+  def __str__(self):
+    return f'{self.data}'
+  
 
 
 class BinaryTree:
   # here we will add the constructor
+  def __init__(self):
+    self.root = None
 
   def insert(self, data):
     '''
-      Insert(data: any) -> None:\n 
+      👶🏻 Insert(data: any) -> None:\n 👶🏻
       creates a new Node from the data passed in and adds it to the tree
       If the data is already in the tree, does not insert it again
     '''
-    pass
+    
+    new_node = Node(data) # 1️⃣ make a node from the data
+    if not self.root: # if there is no root, set node to be root
+      self.root = new_node
+      return
+    
+    current_node = self.root # 2️⃣ loop over the tree starting at root (w/comparisons AlongTheWay)
+    while current_node:
+      if new_node.data < current_node.data: # if data is less than node
+        if not current_node.left: # if there is no left
+          current_node.left = new_node #set the left to be the new node
+          return 
+        else: current_node = current_node.left # else move to the left
+      elif new_node.data > current_node.data: # if the new node data is bigger than the current node
+        if not current_node.right: #check if there is no right
+          current_node.right = new_node  #insert to the right
+          return
+        else: current_node = current_node.right #otherwise we need to move to the right
+
 
   def search(self, val):
     '''
       search(value: any) -> value or bool:\n 
-      Performs depth first search
+      Performs 💥 🔥 DEPTH FIRST SEARCH 🔥 💥
       Search the Tree for a node with the given value
       If the node exists, return it
       If the node doesn't exist, return false
     '''
-    pass
+    if not self.root:# check if the tree is empty
+      return False
+    current_node = self.root #loop through the tree starting at the root
+    while current_node:
+      #if the value is less than the current node
+      if val < current_node.data:
+        # set current node to be the left
+        current_node = current_node.left
+      # else if the value is greater
+      elif val > current_node.data:
+        # set current node to be the right
+        current_node = current_node.right
+      #otherwise return the node
+      else:
+        return current_node
+    return False #in case it all goes wrong
+
 
   def print(self, node=None):
     '''
       print(node=optional: Node) -> None:\n
-      prints out all values recursively (in a breadth first search fashion)
+      prints out all values recursively (in a 🌬 ✨ BREADTH FIRST SEARCH ✨ 💨 fashion)
       defualt start is at root node
     '''
-    pass
+    # check if this is the first recursion
+    if not node: node = self.root
+    #print the node
+    print(node)
+    # if there is a left node
+    if node.left: 
+      # recursively invoke self.print on the node
+      self.print(node.left)
+    # if there is a right node
+    if node.right: 
+      # recurse
+      self.print(node.right)
 
   def size(self, node=None):
     '''
@@ -49,7 +102,21 @@ class BinaryTree:
       Calculate the maximum amount of nodes in any one path from the given node
       If not given a specific node, default to using the root node
     '''
-    pass
+    count_left = 0
+    count_right = 0 
+    if not node:
+      node = self.root
+    max_height = 0
+    def recursive_height(node, height=1):
+      nonlocal max_height
+      if node:
+        if height > max_height:
+          max_height = height
+        recursive_height(node.left, height + 1)
+        recursive_height(node.right, height + 1)
+    recursive_height(node)
+    return max_height
+
 
   def get_max(self):
     '''
@@ -57,7 +124,17 @@ class BinaryTree:
       perform depth first search
       Calculate the maximum value held in the tree
     '''
-    pass
+    #check to see if the tree is empty
+    if not self.root:
+      return False
+    #loop through the tree starting at the root
+    current_node = self.root
+    while current_node.right:
+      current_node = current_node.right
+      #if there is a node to the right
+        # set the current node to be the right
+      #else return value of current node 
+    return current_node.data
 
   def get_min(self):
     '''
@@ -65,4 +142,9 @@ class BinaryTree:
       perform depth first search
       Calculate the minimum value held in the tree
     '''
-    pass
+    if not self.root: return None
+    current_node = self.root
+    while current_node.left:
+      current_node = current_node.left
+    return current_node.data
+
---
+++ main.py
@@ -0,0 +1,22 @@
+from BinaryTree import Node
+from BinaryTree import BinaryTree
+#tester node -- run python3 on main.py
+my_node = Node('hello')
+#print(my_node)
+
+my_BST = BinaryTree()
+
+my_BST.insert(10)
+#print(my_BST.root)
+my_BST.insert(12)
+my_BST.insert(8)
+#print('should be 8', my_BST.root.left)
+#print('should be 12', my_BST.root.right)
+my_BST.print()
+my_BST.insert(15)
+my_BST.insert(9)
+my_BST.insert(3)
+my_BST.insert(21)
+my_BST.insert(7)
+
+print(my_BST.search(21))
