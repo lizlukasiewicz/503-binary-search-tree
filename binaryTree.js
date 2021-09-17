c1bf6c36c23345251a7d207e0ee8355095432682
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2021-09-17 18:31:18 -0400

    binary search tree javascript

--- binaryTree.js
+++ binaryTree.js
@@ -1,91 +1,123 @@
+// Node class
 class Node{
-    constructor(data, left, right){
+    constructor(data){
         this.data = data
-        this.left = left
-        this.right = right
+        this.left = null
+        this.right = null
     }
     info() {
         console.log(this.data, this.left, this.right)
     }
 }
 
-class BinaryTree extends Node{
-    constructor(root, data, left, right){
-        super(data, left, right);
-        this.root = root;
+//binary search tree class
+class BinarySearchTree {
+    constructor(){
+        this.root = null
     }
+    //INSERT(DATA) creates a new node with a value data, if the tree is empty it adds this node to 
+    //a tree and make it root, otherwise call insert(node, data)
     insert(data) {
         let newNode = new Node(data)
-        if (!this.root){
+        if (this.root === null){
             this.root = newNode
+        } else {
+            this.insertNode(this.root, newNode)
         }
-        //⬇️ "catch" for while loop
-        let count = 10
-        let currentNode = this.root
-        while (currentNode < count) {
-            if (newNode.data < currentNode.data) {
-                count--
-                if (!currentNode.left) {
-                    currentNode.left = newNode
-                }
-                else {
-                    currentNode = currentNode.left
-                }
-            } else if (newNode.data > currentNode.data) {
-                count--
-                if (!currentNode.right) {
-                    currentNode.right = newNode
-                }
-                else {
-                    currentNode = currentNode.right
-                }
+    }
+    //INSERTNODE(NODE< NEWNODE) compares given data with the data of current node and moves left or right accordingly 
+    insertNode(node, newNode){
+        if(newNode.data < node.data) {
+            if (node.left === null) {
+                node.left = newNode
+            }else {
+                this.insertNode(node.left, newNode)
+            }
+        } else {
+            if(node.right === null) {
+                node.right = newNode
+            } else {
+                this.insertNode(node.right, newNode)
             }
         }
     }
-    print(node) {
-        if(!node) {
-            node = this.root
-        }
-        console.log(node)
-        if(node.left) {
-            console.log(node.left)
-        }
-        if(node.right) {
-            console.log(node.right)
+    //dearch for node with given data
+    search (node, data) {
+        //if tree is empty, return nullz
+        if (node === null) {
+            return null
+        // ⬇️ if data is less than nodes data, move left ⬇
+        }else if (data < node.data) {
+            return this.search(node.left, data)
+        }//⬇️ if data is more than nodes data, move right
+        else if (data > node.data) {
+            return this.search(node.right, data)
+        // ⬇️ if equal, return node
+        } else {
+            return node
         }
     }
-    height(node){
+    // print(node) {
+    //     if(!node) {
+    //         node = this.root
+    //     }
+    //     console.log(node)
+    //     if(node.left) {
+    //         console.log(node.left)
+    //     }
+    //     if(node.right) {
+    //         console.log(node.right)
+    //     }
+    // }
+    height(node, data){
         let countLeft = 0 
         let countRight = 0
-        if (!node){
+        if (node === null){
             node = this.root
         }
         let maxHeight = 0 
-        
         function recursiveHeight(node, height, maxHeight) {
             height = 1
-            if (node) {
+            if (node !== null) {
                 if (height > maxHeight) {
                     maxHeight = height
                 }
-                recursiveHeight(Node.left, height + 1)
-                recursiveHeight(Node.right, height + 1)
+                recursiveHeight(node.left, height + 1)
+                recursiveHeight(node.right, height + 1)
             }
+            console.log(maxHeight)
         }
         recursiveHeight(node)
         return maxHeight
     }
+    //find the maximum node in a tree, starts from given node
+    findMaxNode(node) {
+        if(node.right === null) {
+            return node
+        } else {
+            return this.findMaxNode(node.right)
+        }
+    }
+    //finds the minimum node in a tree,
+    findMinNode(node) {
+        if (node.left === null) {
+            return node
+        } else {
+            return this.findMinNode(node.left)
+        }
+    }
 }
 
 //100 90 90 80 75 60
-const tree = new BinaryTree()
+const tree = new BinarySearchTree()
 tree.insert(100)
 tree.insert(90)
-tree.insert(90)
+//tree.insert(90)
 tree.insert(80)
-console.log(tree.root)
-// tree.insert(75)
-// tree.insert(60)
-//tree.print()
 
+tree.insert(75)
+tree.insert(60)
+//tree.print()
+//console.log(tree)
+tree.height()
 
